06c1823d1d76733820fb58da538988e8639b4645
Lars <lars@pfrenger.com>
2023-10-23 20:15:59 +0200

    feat(Bot): Add Discord and Config

---
+++ src/Bot/src/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Sparrows.Bot {
+    class Program {
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync() {
+            var configBuilder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config"))
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddJsonFile("appsettings.dev.json", optional: true);
+            IConfigurationRoot config = configBuilder.Build();
+            
+            m_Client = new DiscordSocketClient();
+            m_Client.Log += Log;
+
+            var token = config.GetSection("token").Value;
+
+            await m_Client.LoginAsync(TokenType.Bot, token);
+            await m_Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg) {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+
+        private DiscordSocketClient m_Client;
+    }
+}
